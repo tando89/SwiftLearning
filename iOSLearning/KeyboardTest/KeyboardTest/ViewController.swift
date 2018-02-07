0a433027b8d998a8b9ce881b1bbecbd1b9a27526
Tan Do <tan@Tans-Mac-mini.local>
2018-02-06 19:42:51 -0800

    added KeyboardTest app, using IQKeyBoard pod, and scrollview

---
+++ iOSLearning/KeyboardTest/KeyboardTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyboardTest
+//
+//  Created by Tan Do  on 2/6/18.
+//  Copyright © 2018 Tan Do . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
